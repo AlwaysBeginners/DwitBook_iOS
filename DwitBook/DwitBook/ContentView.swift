3d8bd829422c9e763d2080fdb27f5cb09905d94a
jsangyun <jsangyun1039@naver.com>
2022-04-04 01:26:42 +0900

    initial commit

---
+++ DwitBook/DwitBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DwitBook
+//
+//  Created by 정상윤 on 2022/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DwitBook/DwitBook/DwitBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DwitBookApp.swift
+//  DwitBook
+//
+//  Created by 정상윤 on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct DwitBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
