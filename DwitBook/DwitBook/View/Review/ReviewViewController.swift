a569dca3166b020c2413914502033cea131c325c
jsangyun <jsangyun1039@naver.com>
2022-04-19 01:34:07 +0900

    ReviewViewController IBOutlets 추가

--- DwitBook/DwitBook/View/Review/ReviewViewController.swift
+++ DwitBook/DwitBook/View/Review/ReviewViewController.swift
@@ -2,25 +2,12 @@
 //  DwitBook
 
 import UIKit
-import RxSwift
 
 class ReviewViewController: UIViewController {
-    
-    var bookId: Int!
-    
-    @IBOutlet weak var imageContainerView: UIView!
-    @IBOutlet weak var bookThumbnailImageView: UIImageView!
-    @IBOutlet weak var bookTitleLabel: UILabel!
-    @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var publishedYearLabel: UILabel!
-    
-    let viewModel = ReviewViewModel()
-    let disposeBag = DisposeBag()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
 
 }
